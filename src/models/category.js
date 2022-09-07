4e74a136fb490c9c2c6094120763ea6c3caaf2ff
OliverosA <aldair_oliveros09@hotmail.es>
2022-09-07 12:44:52 -0600

    starting category transactions with DB

--- src/models/category.js
+++ src/models/category.js
@@ -1,7 +1,25 @@
 const { pool } = require('../utils/db');
 
-module.exports.create = ({ person, name, description }) => {};
+module.exports.create = ({ person, name, description }) => {
+  const bindings = { person, name, description };
+  const SQL_INSERT_CATEGORY = `INSERT INTO CATEGORY(CATEGORY, NAME, DESCRIPTION, PERSON)
+                               VALUES(SQ_CATEGORY.NEXTVAL, :name, :description, :person)`;
+  return pool(SQL_INSERT_CATEGORY, bindings, { autoCommit: true });
+};
 
-module.exports.fetchAll = () => {};
+module.exports.fetchAll = ({ person }) => {
+  const bindings = { person };
+  const SQL_SELECT_CATEGORIES = `SELECT CATEGORY, NAME, DESCRIPTION, TO_CHAR(ADD_DATE, 'YYYY-MM-DD') AS ADD_DATE
+                                FROM CATEGORY
+                                WHERE PERSON = :person`;
+  return pool(SQL_SELECT_CATEGORIES, bindings);
+};
 
-module.exports.findById = ({ category }) => {};
+module.exports.findById = ({ person, category }) => {
+  const bindings = { person, category };
+  const SQL_SELECT_CATEGORY = `SELECT CATEGORY, NAME, DESCRIPTION, TO_CHAR(ADD_DATE, 'YYYY-MM-DD') AS ADD_DATE
+                                FROM CATEGORY
+                                WHERE PERSON = :person
+                                AND CATEGORY = :category`;
+  return pool(SQL_SELECT_CATEGORY, bindings);
+};

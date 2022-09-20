44fa07a87f330399404619a5e0ecea6ea28f07bd
OliverosA <aldair_oliveros09@hotmail.es>
2022-09-19 22:55:39 -0600

    continuing with bankAccount files

--- src/controllers/bankaccount.js
+++ src/controllers/bankaccount.js
@@ -1,7 +1,6 @@
 const BankAccount = require("../models/bankaccount");
 
 module.exports.createBankAccount = async (req, res, next) => {
-  //person, account_number, balance, currency
   const args = {
     person: req.person.person,
     account_number: req.body.account_number,
@@ -11,6 +10,31 @@ module.exports.createBankAccount = async (req, res, next) => {
   try {
     await BankAccount.create(args);
     res.status(200).json({ message: "Bank Account created!" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+module.exports.getBankAccounts = async (req, res, next) => {
+  const args = {
+    person: req.person.person,
+  };
+  try {
+    const { rows } = await BankAccount.fetchAll(args);
+    res.status(200).json({ data: rows });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+module.exports.getBankAccount = async (req, res, next) => {
+  const args = {
+    person: req.person.person,
+    bankaccount: Number(req.params.id),
+  };
+  try {
+    const { rows } = await BankAccount.fetchById(args);
+    res.status(200).json({ data: rows });
   } catch (error) {
     res.status(400).json({ message: error });
   }
--- src/routes/bankaccount.js
+++ src/routes/bankaccount.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const guard = require("../guard/guard");
 
-const { createBankAccount } = require("../controllers/bankaccount");
+const {
+  createBankAccount,
+  getBankAccounts,
+  getBankAccount,
+} = require("../controllers/bankaccount");
 
 router.post("/bankaccount", guard, createBankAccount);
+router.get("/bankaccount", guard, getBankAccounts);
+router.get("/bankaccount/:id", guard, getBankAccount);
 
 module.exports = router;

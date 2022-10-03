2265b7e5e8a549c81dd10896a539f50778b6ad70
OliverosA <aldair_oliveros09@hotmail.es>
2022-10-03 00:37:47 -0600

    update to transfer query

--- src/controllers/transfer.js
+++ src/controllers/transfer.js
@@ -2,11 +2,13 @@ const Transfer = require("../models/transfer");
 
 module.exports.createTransfer = async (req, res, next) => {
   const args = {
+    person: req.person.person,
     amount: req.body.amount,
     description: req.body.description,
     orig_account: req.body.orig_account,
     dest_account: req.body.dest_account,
   };
+  console.log(args);
   try {
     await Transfer.create(args);
     res.status(200).json({ message: "Transfer created!" });
